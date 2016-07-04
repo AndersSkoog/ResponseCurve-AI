cc0602a72e990531fad8e9b6b638048826ed8dc3
AndersSkoog <anders.skooog@gmail.com>
2016-07-04 07:07:50 +0200

    Delete RC_COMPONENT.cs

--- Components/RC_COMPONENT.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using UnityEditor;
-using System.Collections;
-
-public class RC_COMPONENT : MonoBehaviour
-{
-	public virtual void ON_INSPECTOR_GUI(){}
-	public virtual void ON_SCENE_GUI(){}
-}
